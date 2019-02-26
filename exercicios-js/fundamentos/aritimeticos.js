1d67b76ae8f0db331c3d40a2e9384e6f36d2e5ca
JoseCarlos <josecarlos.n.f99@gmail.com>
2019-02-26 19:40:32 -0300

    Operadores Aritimeticos

---
+++ exercicios-js/fundamentos/aritimeticos.js
@@ -0,0 +1,12 @@
+/* Operadores aritimeticos são binario, pos operam sempre
+com dois operandos */
+
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtraçcao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtraçcao, multiplicacao, divisao, modulo)
