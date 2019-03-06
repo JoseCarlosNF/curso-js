0a6d9a85917d179935bffd969a0fb50a9ab73407
José Carlos <josecarlos.n.f99@gmail.com>
2019-03-06 19:38:54 -0300

    Funções: Tipos de Declaração(Named/Expression/Declaration)

---
+++ exercicios-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,13 @@
+console.log(soma(3, 4))
+
+
+// Function Declaration
+function soma(x, y) { return x + y }
+
+// Function Expression
+const sub = function (x, y) { return x - y }
+console.log(sub(3, 4))
+
+// Named function expression
+const mult = function mult(x, y) { return x * y }
+console.log(mult(3, 4))
