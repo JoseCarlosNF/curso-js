328e022b8d745cf227da8c22914d62672c84be10
José Carlos <josecarlos.n.f99@gmail.com>
2019-03-07 18:23:38 -0300

    Introdução a Orientação a Objetos

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL !!!
+
+// Procedural (Procedure)
+procesamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto. Antes era a função
+
+// Principios Importantes:
+// 1. Abstração: Tradução do mundo real para o código. Simplificando o obj
+// 2. Encapsulamento: Não mostra mais que o necessário
+// 3. Herança (Prototype)
+// 4. Polimorfismo: Multiplas formas
