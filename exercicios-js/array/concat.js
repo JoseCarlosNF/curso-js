4d182dc96b424c3523a456644b025441de5c55e9
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2019-03-11 19:16:42 -0300

    Array: ⭐ Demonstração concat

---
+++ exercicios-js/array/concat.js
@@ -0,0 +1,9 @@
+// ⭐ array.concat
+// ✅ concatena os parametros, gerando um novo array
+
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uoxinton', 'Uesclei']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2], [3,4], 5))
