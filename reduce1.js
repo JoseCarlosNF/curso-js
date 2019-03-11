19635dc7149139300252c59c6cfba43fc6532a10
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2019-03-11 18:13:40 -0300

    Array: ⭐ Demonstração reduce

---
+++ reduce1.js
@@ -0,0 +1,15 @@
+const alunos = [
+  { nome: 'João', nota: 7.3, bolsista: false},
+  { nome: 'Maria', nota: 9.2, bolsista: true},
+  { nome: 'Pedro', nota: 9.8, bolsista: false},
+  { nome: 'Ana', nota: 8.7, bolsista: true}
+]
+
+// ⭐ array.reduce
+// ✅ faz uma acumalção de valores de array
+const resultado = alunos.map(aluno => aluno.nota).reduce(function(acumulador, atual) {
+  console.log(acumulador, atual)
+  return acumulador + atual
+}, 10)
+
+console.log(resultado)
