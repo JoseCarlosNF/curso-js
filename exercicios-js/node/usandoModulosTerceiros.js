f4e5ddfdb0bf5558c45890e41f8bd5ef5690f5d5
José Carlos <josecarlos.n.f99@gmail.com>
2019-05-27 18:17:55 +0000

    Retorno do curso, através do gitpod

--- exercicios-js/node/usandoModulosTerceiros.js
+++ exercicios-js/node/usandoModulosTerceiros.js
@@ -1,2 +1,4 @@
+// pacotes instalados: nodemon (refresh automatico), lodash (canivete suiço, várias funções)
+
 const _ = require('lodash')
-setInterval( () => console.log(_.random(0, 5)), 2000)
+setInterval( () => console.log(_.random(10, 300)), 500)
